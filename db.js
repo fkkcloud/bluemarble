a6493c14b4c79cce2bb8b0f4827ff4456e5ba123
Jae Hyun Yoo <fkkcloud@gmail.com>
2015-08-06 08:49:39 -0700

    db update

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
+var url = process.env.MONGOLAB_URI || 'mongodb://localhost/bluemarbledb';
+mongoose.connect(url);
+
+/*
 mongoose.connect('mongodb://localhost/lastsex', function(){
 	console.log('mongodb connected')
 });
+*/
 
 module.exports = mongoose;
