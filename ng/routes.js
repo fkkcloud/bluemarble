f2962ad3f408f8a113b6fdee5db564b0af7ab870
Jae Hyun Yoo <fkkcloud@gmail.com>
2015-08-09 06:00:36 -0700

    pushing adsense policy stuff

--- ng/routes.js
+++ ng/routes.js
@@ -22,6 +22,21 @@ angular.module('app')
 			controller: 'AccountCtrl',
 			templateUrl: '/templates/account.html'
 		})
+	.when('/privacy',
+		{
+			controller: 'AccountCtrl',
+			templateUrl: '/templates/policy.html'
+		})
+	.when('/aboutus',
+		{
+			controller: 'AccountCtrl',
+			templateUrl: '/templates/aboutus.html'
+		})
+	.when('/contact',
+		{
+			controller: 'AccountCtrl',
+			templateUrl: '/templates/contactus.html'
+		})
 	// Redirect to the root page.
 	
 	.otherwise(
